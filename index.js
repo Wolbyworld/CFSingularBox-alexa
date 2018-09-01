c40f9ed261a09d2a7f3be84a544962fc5f4b79f5
Wolbyworld <a.martinezhiges@gmail.com>
2018-09-01 17:12:23 +0200

    Cleaned up from previous version

--- index.js
+++ index.js
@@ -3,8 +3,15 @@
 
 const Alexa = require('ask-sdk');
 const RSSFeed = "http://fetchrss.com/rss/5b816db48a93f882278b4567560933858.xml";
+
+//Variable definition
+const SKILL_NAME = 'Crossfit Singular Box - El WOD de hoy';
+const HELP_MESSAGE = 'Puedes decir, dime entreno del dia';
+const HELP_REPROMPT = '¿Cómo te puedo ayudar?';
+const STOP_MESSAGE = 'Adios gerrero';
+
 var speechOutput = ''
- var string2read = ''
+var string2read = ''
 
 
 const NuevaInformacionHandler = {
@@ -66,26 +73,6 @@ function cleanString(_text) {
   return  temp;
 }
 
-// ------- From the old
-const GetNewFactHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'LaunchRequest'
-      || (request.type === 'IntentRequest'
-        && request.intent.name === 'GetNewFactIntent');
-  },
-  handle(handlerInput) {
-    const factArr = data;
-    const factIndex = Math.floor(Math.random() * factArr.length);
-    const randomFact = factArr[factIndex];
-    const speechOutput = GET_FACT_MESSAGE + randomFact;
-    return handlerInput.responseBuilder
-      .speak(speechOutput)
-      .withSimpleCard(SKILL_NAME, randomFact)
-      .getResponse();
-  },
-};
-
 const HelpHandler = {
   canHandle(handlerInput) {
     const request = handlerInput.requestEnvelope.request;
@@ -140,43 +127,12 @@ const ErrorHandler = {
   },
 };
 
-const SKILL_NAME = 'Crossfit Singular Box - El WOD de hoy';
-const GET_FACT_MESSAGE = 'Here\'s your fact: ';
-const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
-const HELP_REPROMPT = 'What can I help you with?';
-const STOP_MESSAGE = 'Goodbye!';
-
-const ES_GET_FACT_MESSAGE = 'Aquí va la culturilla de salon: ';
-
-
-const data = [
-  'A year on Mercury is just 88 days long.',
-  'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-  'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-  'On Mars, the Sun appears about half the size as it does on Earth.',
-  'Earth is the only planet not named after a god.',
-  'Jupiter has the shortest day of all the planets.',
-  'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-  'The Sun contains 99.86% of the mass in the Solar System.',
-  'The Sun is an almost perfect sphere.',
-  'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-  'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-  'The temperature inside the Sun can reach 15 million degrees Celsius.',
-  'The Moon is moving approximately 3.8 cm away from our planet every year.',
-];
-
-const ESdata = [
-  'Alvaro es el puto amo.',
-  'Aloña es una mamuza.',
-];
-
 
 const skillBuilder = Alexa.SkillBuilders.custom();
 
 exports.handler = skillBuilder
   .addRequestHandlers(
     NuevaInformacionHandler,
-    GetNewFactHandler,
     HelpHandler,
     ExitHandler,
     SessionEndedRequestHandler
